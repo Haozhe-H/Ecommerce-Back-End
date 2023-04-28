387c51fe6628df6dfdd8c86b23d6143ead3f05a9
Haozhe-H <haozhehuang@outlook.com>
2023-04-28 01:00:17 -0400

    update tag post

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -49,6 +49,14 @@ router.get("/:id", (req, res) => {
 
 router.post("/", (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+    .then((newTag) => {
+      res.status(200).json(newTag);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 router.put("/:id", (req, res) => {
