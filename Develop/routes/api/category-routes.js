608024abdc93278f8358e2596a13c6a523a00b9e
Haozhe-H <haozhehuang@outlook.com>
2023-04-27 23:27:57 -0400

    update update category route

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -1,43 +1,82 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // find all categories
   // be sure to include its associated Products
   Category.findAll({
-    attributes: ['id', 'category_name'],
-    include: [{
-      model: Product,
-      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
-    }]
-  }).then((category) => {
-    res.json(category)
+    attributes: ["id", "category_name"],
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
+      },
+    ],
   })
+    .then((category) => {
+      res.json(category);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json(err);
+    });
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-  
-});
-
-router.post('/', (req, res) => {
-  // create a new category
-  Category.create(req.body).then((newCategory) => {
-    res.status(200).json(newCategory)
-  }).catch((err) => {
-    console.log(err);
-    res.status(400).json(err)
+  Category.findByPk(req.params.id, {
+    attributes: ["id", "category_name"],
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
+      },
+    ],
   })
+    .then((category) => {
+      res.json(category);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json(err);
+    });
 });
 
-router.put('/:id', (req, res) => {
+router.post("/", (req, res) => {
+  // create a new category
+  Category.create(req.body)
+    .then((newCategory) => {
+      res.status(200).json(newCategory);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+router.put("/:id", (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then(() => {
+      return Category.findByPk(req.params.id);
+    })
+    .then((updatedCategory) => {
+      res.status(200).json(updatedCategory);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
 });
 
